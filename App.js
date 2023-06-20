1627a27917a0d9f79e46a3f5cd36eaa61dbd024d
mnavarro1688 <129029524+mnavarro1688@users.noreply.github.com>
2023-06-19 20:34:28 -0600

    update

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import CameraScreen from './components/camerascreen';
 
 const App = () => {
   return (
-    <View style={{ flex: 1 }}>
-      <Text>Take a picture</Text>
+    <View style={styles.container}>
       <CameraScreen />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
 
 export default App;
--- components/camerascreen.js
+++ components/camerascreen.js
@@ -1,26 +1,56 @@
-import React, { useEffect, useRef } from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
 import { Camera } from 'expo-camera';
-
+import * as FileSystem from 'expo-file-system';
 
 const CameraScreen = () => {
   const cameraRef = useRef(null);
+  const [hasPermission, setHasPermission] = useState(null);
 
   useEffect(() => {
     (async () => {
       const { status } = await Camera.requestPermissionsAsync();
-      if (status !== 'granted') {
-        alert('Se requieren permisos de cámara para utilizar esta función.');
-      }
+      setHasPermission(status === 'granted');
     })();
   }, []);
 
+  const takePicture = async () => {
+    if (cameraRef.current) {
+      const photo = await cameraRef.current.takePictureAsync();
+      const photoUri = photo.uri;
+
+      const destinationUri = FileSystem.documentDirectory + 'capturedPhoto.jpg'; // Ruta específica para guardar la foto
+
+      try {
+        await FileSystem.moveAsync({
+          from: photoUri,
+          to: destinationUri,
+        });
+        console.log('Foto guardada en:', destinationUri);
+      } catch (error) {
+        console.error('Error al guardar la foto:', error);
+      }
+    }
+  };
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No tienes permisos para acceder a la cámara.</Text>;
+  }
+
   return (
     <View style={{ flex: 1 }}>
-      <Camera style={{ flex: 1 }} type={Camera.Constants.Type.back} ref={cameraRef} />
+      <Camera style={{ flex: 1 }} type={Camera.Constants.Type.back} ref={cameraRef}>
+        <View style={{ flex: 1, backgroundColor: 'transparent', flexDirection: 'row' }}>
+          <TouchableOpacity style={{ alignSelf: 'flex-end', alignItems: 'center' }} onPress={takePicture}>
+            <Image source={require('./camera_icon.png')} style={{ width: 50, height: 50 }} />
+          </TouchableOpacity>
+        </View>
+      </Camera>
     </View>
   );
-
 };
 
 export default CameraScreen;
